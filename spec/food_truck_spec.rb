f918fc866fc5dfb02013ae964a74102f7e45ef52
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-04-26 11:18:54 -0400

    Finish iteration 1

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -16,11 +16,21 @@ RSpec.describe FoodTruck do
 
   it "can check the stock for a given item" do
     expect(food_truck.check_stock(item1)).to eq(0)
+
+    food_truck.stock(item1, 30)
+    expect(food_truck.check_stock(item1)).to eq(30)
+
+    food_truck.stock(item1, 25)
+    expect(food_truck.check_stock(item1)).to eq(55)
   end
 
   it "can add stock" do
     food_truck.stock(item1, 30)
-
     expect(food_truck.inventory).to eq({item1 => 30})
+
+    food_truck.stock(item2, 12)
+    expect(food_truck.inventory).to eq({item1 => 30,
+                                        item2 => 12})
   end
+  
 end
