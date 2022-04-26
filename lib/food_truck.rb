88513742fa7683cb3993d3d2c2f30252783febe9
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-04-26 11:16:34 -0400

    Add stock and check stock methods

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -4,8 +4,18 @@ class FoodTruck
 
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
   end
+
+
+  def check_stock(item)
+      @inventory[item]
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
+  end
+
 
 
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -2,6 +2,8 @@ require './lib/food_truck'
 
 RSpec.describe FoodTruck do
   let(:food_truck) {FoodTruck.new("Rocky Mountain Pies")}
+  let(:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
+  let(:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
 
   it "exists" do
     expect(food_truck).to be_a(FoodTruck)
@@ -10,5 +12,15 @@ RSpec.describe FoodTruck do
   it "has readable attributes" do
     expect(food_truck.name).to eq("Rocky Mountain Pies")
     expect(food_truck.inventory).to eq({})
+  end
+
+  it "can check the stock for a given item" do
+    expect(food_truck.check_stock(item1)).to eq(0)
+  end
+
+  it "can add stock" do
+    food_truck.stock(item1, 30)
+
+    expect(food_truck.inventory).to eq({item1 => 30})
   end
 end
